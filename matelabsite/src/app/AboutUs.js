9a9e2fea73eb9279a5f3b21e015d9b4d9edc9a52
ACaminos <caminosariel@gmail.com>
2020-03-01 19:32:20 -0300

    perfiles casi OK

--- matelabsite/src/app/AboutUs.js
+++ matelabsite/src/app/AboutUs.js
@@ -8,27 +8,27 @@ const arrayIntegrantes = [
     site: "http://www.jpromano.net",
     git: "https://www.github.com/jpromanonet",
     descripcion:
-      "DevOps, especializado en ciencia de datos, licenciado en Matemática de la UBA, MVP 2017 en Microsoft SQL, mí rol es resolver problemas de arquitectura en nuestros desarrollos e implementar nuevas tecnologías.",
+      "DevOps, mí rol es resolver problemas de arquitectura en nuestros desarrollos e implementar nuevas tecnologías, vivo a mate y mi frase es 'Usted es diabolico señor Simpson'.",
     photo: "/perfiles/perfil_juanp.png"
+  },
+  {
+    name: "Martin J. Pared Baez",
+    job: "Desarrollador Full Stack",
+    linkedin: "",
+    site: "",
+    git: "https://github.com/baubyte",
+    descripcion: "DevOps, especializado en backend, base de datos y algo de FrontEnd, el menos sociable del grupo.",
+    photo: "/perfiles/perfil_tincho.jpg"
   },
   {
     name: "Maximiliano Salamone",
     job: "Referente Comercial",
     linkedin:
       "https://www.linkedin.com/in/maximiliano-a-salamone-castro-7a033733/",
-    site: "",
-    git: "",
+    site: "http://qboit.com/",
+    git: "https://github.com/maxisalamone",
     descripcion:
       "DevOps, con especialidad en implementacion en la nube, backend, base de datos. Licenciado en Relaciones Intenacionales, en pos del costado humano de nuestros desarrollos.",
-    photo: "/perfiles/perfil_maxi.jpg"
-  },
-  {
-    name: "Martin Pared Baez",
-    job: "Desarrollador Full Stack",
-    linkedin: "",
-    site: "",
-    git: "https://github.com/baubyte",
-    descripcion: "",
     photo: "/perfiles/perfil_maxi.jpg"
   },
   {
@@ -42,32 +42,32 @@ const arrayIntegrantes = [
     photo: "/perfiles/perfil_gaby.jpg"
   },
   {
-    name: "Ariel Caminos",
-    job: "Desarrollador Full Stack",
-    linkedin: "https://www.linkedin.com/in/ariel-caminos-3b187863/",
-    site: "",
-    git: "https://github.com/ACaminos",
-    descripcion: "",
-    photo: "/perfiles/perfil_ariel.jpg"
-  },
-  {
-    name: "Damian Barbero",
-    job: "Desarrollador Full Stack",
-    linkedin: "https://www.linkedin.com/in/damian-barbero/",
-    site: "",
-    git: "https://github.com/DamianB96",
-    descripcion: "",
-    photo: "/perfiles/perfil_maxi.jpg"
-  },
-  {
     name: "Rosana Caballero",
     job: "Referente de Gestion",
     linkedin: "",
     site: "",
     git: "",
-    descripcion: "",
-    photo: "/perfiles/perfil_maxi.jpg"
-  }
+    descripcion: "Licenciada en Administración de Recursos Humanos. Soy la que lleva el papeleo y registro contable de este grupo, la que quiere todo en regla.",
+    photo: "/perfiles/perfil_ro.jpg"
+  },
+  {
+    name: "Ariel Caminos",
+    job: "Desarrollador Full Stack",
+    linkedin: "https://www.linkedin.com/in/ariel-caminos-3b187863/",
+    site: "",
+    git: "https://github.com/ACaminos",
+    descripcion: "Desarrollador full stack, conocimientos en C, Java, PHP, html, css, SQL. Proactivo, con actitud siempre positiva. Amplia experiencia en el trato con el usuario/cliente. A la grande le puse cuca",
+    photo: "/perfiles/perfil_ariel.jpg"
+  },
+  {
+    name: "Martin J. Pared Baez",
+    job: "Desarrollador Full Stack",
+    linkedin: "",
+    site: "",
+    git: "https://github.com/baubyte",
+    descripcion: "DevOps, especializado en backend, base de datos y algo de FrontEnd, el menos sociable del grupo.",
+    photo: "/perfiles/perfil_tincho.jpg"
+  },
 ];
 
 const AboutUs = props => {
@@ -95,8 +95,15 @@ const AboutUs = props => {
                     <i className="material-icons right">more_vert</i>
                   </span>
                   <p>
+                    <a href={`${index.git}`} target="_blank">
+                    <i className="fa fa-github"></i>
+                    </a>&nbsp;                
                     <a href={`${index.linkedin}`} target="_blank">
-                      Linkedin
+                    <i className="fa fa-linkedin"></i>
+                    </a>
+                    &nbsp;                
+                    <a href={`${index.site}`} target="_blank">
+                    <i className="fa fa-link"></i>
                     </a>
                   </p>
                 </div>
