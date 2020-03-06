26bb0eea213037d1f4fa629fe82aa5985f2dd4a3
jpromanonet <programming@jpromano.net>
2020-03-06 11:35:42 -0300

    Location arrow gone

--- matelabsite/src/app/footer.js
+++ matelabsite/src/app/footer.js
@@ -1,16 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const footer = props => {
-    return (
-       
-        <div className="col-md-3 company-details">
-            <div className="icon-top red-text"></div>
-            <div>
-                <i className="fa fa-location-arrow"></i>
-            </div>
-        </div>
-
-    )
-}
+  return <div className="col-md-3 company-details"></div>;
+};
 
 export default footer;
