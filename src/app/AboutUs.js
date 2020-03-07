82c519b66164b6a617afa9f2510d3c84564b073f
jpromanonet <programming@jpromano.net>
2020-03-07 20:48:56 -0300

    Spelling corrections

--- src/app/AboutUs.js
+++ src/app/AboutUs.js
@@ -2,31 +2,31 @@ import React from "react";
 
 const arrayIntegrantes = [
   {
-    name: "Gabriel D. Lombardo",
+    name: "Gabriel Lombardo",
     job: "Desarrollador Front End",
     linkedin: "https://www.linkedin.com/in/gabriel-dario-lombardo-1531b125/",
     git: "https://github.com/gaby1986",
     descripcion:
-      "Tras años de experiencia en distintos medios digitales. Adquiri herramientas como desarrollador Front-End y motivación a seguir buscando nuevos desafíos como programador. ",
+      "Tras años de experiencia en distintos medios digitales, adquirí herramientas como desarrollador FrontEnd y motivación para seguir buscando nuevos desafíos como programador.",
     photo: "/perfiles/perfil_gaby.jpg"
   },
   {
-    name: "Juan P. Romano",
+    name: "Juan Romano",
     job: "Desarrollador Full Stack",
     linkedin: "https://www.linkedin.com/in/jupromano/",
     site: "http://jpromano.net",
     git: "https://www.github.com/jpromanonet",
     descripcion:
-      "DevOps(No me gustan los titulos), mí rol es resolver problemas de arquitectura en nuestros desarrollos e implementar nuevas tecnologías, vivo a mate(?).",
+      "Matemático, desarrollador full stack, mí rol es programar, diseñar y resolver problemas de arquitectura e implementar nuevas tecnologías, vivo a mate(?).",
     photo: "/perfiles/perfil_juanp.png"
   },
   {
-    name: "Martin J. Pared Baez",
+    name: "Martin Pared Baez",
     job: "Desarrollador Full Stack",
     linkedin: "",
     git: "https://github.com/baubyte",
     descripcion:
-      "DevOps, especializado en backend, base de datos y algo de FrontEnd, el menos sociable del grupo.",
+      "DevOps, especializado en BackEnd, base de datos y algo de FrontEnd, el menos sociable del grupo.",
     photo: "/perfiles/perfil_tincho.jpg"
   },
   {
@@ -38,6 +38,15 @@ const arrayIntegrantes = [
     photo: "/perfiles/perfil_ro.jpg"
   },
   {
+    name: "Ariel Caminos",
+    job: "Desarrollador Full Stack",
+    linkedin: "https://www.linkedin.com/in/ariel-caminos-3b187863/",
+    git: "https://github.com/ACaminos",
+    descripcion:
+      "Desarrollador full stack, conocimientos en C, JAVA, PHP, HTML, CSS, SQL. Pro activo, con actitud siempre positiva. Amplia experiencia en el trato con el usuario/cliente. A la grande le puse cuca.",
+    photo: "/perfiles/perfil_ariel.jpg"
+  },
+  {
     name: "Maximiliano Salamone",
     job: "Referente Comercial",
     linkedin:
@@ -45,17 +54,8 @@ const arrayIntegrantes = [
     site: "http://qboit.com/",
     git: "https://github.com/maxisalamone",
     descripcion:
-      "DevOps, con especialidad en implementacion en la nube, backend, base de datos. Licenciado en Relaciones Intenacionales, en pos del costado humano de nuestros desarrollos.",
+      "DevOps, con especialidad en implementación en la nube, BackEnd, base de datos. Licenciado en Relaciones Internacionales, en pos del costado humano de nuestros desarrollos.",
     photo: "/perfiles/perfil_maxi.jpg"
-  },
-  {
-    name: "Ariel Caminos",
-    job: "Desarrollador Full Stack",
-    linkedin: "https://www.linkedin.com/in/ariel-caminos-3b187863/",
-    git: "https://github.com/ACaminos",
-    descripcion:
-      "Desarrollador full stack, conocimientos en C, Java, PHP, html, css, SQL. Proactivo, con actitud siempre positiva. Amplia experiencia en el trato con el usuario/cliente. A la grande le puse cuca",
-    photo: "/perfiles/perfil_ariel.jpg"
   }
 ];
 
--- src/app/Frontpage.js
+++ src/app/Frontpage.js
@@ -38,14 +38,14 @@ class Frontpage extends Component {
         <Services
           id="services"
           title1="Servicios"
-          text1="Somos creadores de soluciones tecnológicas para nuestros clientes, enfocandonos en su satisfacción junto con una rapida respuesta de manera continua, brindandoles servicios de desarrollo de software, soporte IT, Infraestructura, Business Inteligence y Data Science"
-          text2="Creamos soluciones a partir de tres principios fundamentales: las personas, los procesos y los productos, lo que nos permite generar valor agregado a las organizaciones de quienes nos eligen."
+          text1="Somos creadores de soluciones tecnológicas para nuestros clientes, enfocándonos en su satisfacción junto con una rápida respuesta de manera continua, brindándoles servicios de desarrollo de Software, Soporte IT, Infraestructura, Business Inteligence y Data Science."
+          text2="Creamos soluciones a partir de tres principios fundamentales: las personas, los procesos y los productos, que nos permite generar valor agregado a las organizaciones de quienes nos eligen."
         ></Services>
         <Organization
           id="organization"
           title1="Organización"
-          text1="Somos una cooperativa de tecnologia sin jerarquias, organizada horizontalmente de forma colaborativa y participativa, donde cada persona tiene un voto y la palabra de todos tiene el mismo valor, no hay jefes, ni CEO, ni CTO ni puestos directivos, porque la idea es trabajar de lo que nos apasiona y divertirnos creando soluciones tecnologicas a problemas desafiantes y fascinantes."
-          text2="Matelab es una espacio de trabajo donde todos son escuchados por igual, todo se puede debatir y votar ya que lo importante para nosotros son ante todo las personas y por eso nuestra forma de trabajar se basa en cuatro principios tales como la honestidad, la inclusión, la calidad humana y la excelencia técnica."
+          text1="Somos una cooperativa de tecnología sin jerarquías, organizada horizontalmente de forma colaborativa y participativa, donde cada persona tiene un voto y la palabra de todos tiene el mismo valor, no hay jefes, ni CEO, ni CTO, ni puestos directivos, porque la idea es trabajar de lo que nos apasiona y divertirnos creando soluciones tecnológicas a problemas desafiantes y fascinantes."
+          text2="Matelab es una espacio de trabajo donde todos son escuchados por igual, todo se puede debatir y votar ya que lo importante para nosotros son ante todo las personas con sus diferentes maneras de pensar y por eso nuestra forma de trabajar se basa en cuatro pilares tales como la honestidad, la inclusión, la calidad humana y la excelencia técnica."
         ></Organization>
         <Aboutus id="aboutus" title1="Sobre Nosotros"></Aboutus>
         <Customer id="clientes" title1="Confiaron en Nosotros"></Customer>
