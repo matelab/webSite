e8732810da566cddd7dc164beaa1208a8e5a34cb
jpromanonet <programming@jpromano.net>
2020-08-15 16:27:33 -0600

    Organization

--- en/src/app/Organization.js
+++ en/src/app/Organization.js
@@ -8,6 +8,7 @@ const Organization = props => {
               <div className="col s12">
                 <p className="paragraph1">{props.text1}</p>
                 <p className="paragraph1">{props.text2}</p>
+                <p className="paragraph1">{props.text3}</p>
               </div>
             </div>
         </div>
