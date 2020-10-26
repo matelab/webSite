e1f7169ea60305300e151bc9c52caee1df8e2e7c
jpromanonet <programming@jpromano.net>
2020-10-26 17:38:09 -0300

    Organization

--- en/src/app/Frontpage.js
+++ en/src/app/Frontpage.js
@@ -69,7 +69,7 @@ class Frontpage extends Component {
           id="organization"
           title1="Organization"
           text1="We are a workers cooperative because we believe that the people have to be able to voluntarily join efforts and achieve ecominical, social and cultural aspirations, so we choose to build a totally democratic and shared organization with a non-hierarchical horizontal structure."
-          text2="Matelab is a workspace where everyone listen to each other as equals, everything is open for debate(doesn't mean that hate speach is able at all) and vote because for us the most important is the people with their different ways of thinking and because of that our work ethic is based on four fundamental principles... honesty, inclusion, human quality and technical excelence."
+          text2="Matelab is a workspace where everyone listen to each other as equals, everything is open for debate(doesn't mean that hate speech is able or has space in Matelab at all) and vote because for us the most important is the people with their different ways of thinking and because of that our work ethic is based on four fundamental principles... honesty, inclusion, human quality and technical excelence."
         ></Organization>
         <Aboutus id="aboutus" title1="About us"></Aboutus>
         <Customer id="clientes" title1="Who trusted us"></Customer>
