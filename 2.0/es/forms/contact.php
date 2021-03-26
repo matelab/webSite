ff631118006dec34852eb4ddc1cf58662d58df13
juanPabloCesarini <juanpcesarini@hotmail.com>
2021-03-26 17:28:14 -0300

    Configuración email

---
+++ 2.0/es/forms/contact.php
@@ -0,0 +1,29 @@
+<?php
+    include('cuerpoMail.php');
+    $nombre = $_POST['name'];
+    $email = $_POST['email'];
+    $asunto = $_POST['subject'];
+    $mensaje = $_POST['message'];
+
+    $mailUsuario = new phpmailer();
+    $mailUsuario->PluginDir = "../includesMails/";
+    try {
+        //Server settings
+        $mailUsuario->isSMTP();     
+        $mailUsuario->SMTPAuth   = true;                          // Enable SMTP authentication
+        $mailUsuario->Host       = 'mail.xxx.ferozo.com';    // Set the SMTP server to send through
+        $mailUsuario->Username   = 'contacto@matelab.com.ar'; // SMTP username
+        $mailUsuario->Password   = ' ';              // SMTP password
+        $mailUsuario->Port       = 587;
+        $mailUsuario->SetFrom('contacto@matelab.com.ar','Matelab Software Factory');
+        $mailUsuario->AddAddress($email); 
+        // Content
+        $mailUsuario->IsHTML(true);                                  // Set email format to HTML
+        $mailUsuario->Subject     = "Visitante mateWEB";
+        $mailUsuario->Body        =  $cuerpoMail;
+        $mailUsuario->send();
+        //header('location: ../vistas/home.php'); Levantamos un alert con "mensaje enviado"???
+    } catch (Exception $e) {
+        echo "Error: {$e->getMessage()}";
+    }
+?>
